b2dbd220e077a72dfa1d7d0d291af0c92121d543
jotasv <jsveleda@inf.ufsm.br>
2021-08-19 00:11:47 -0300

    log de criação de campanha adicionado

--- app/UseCases/Campaign/CreateCampaignUseCase.js
+++ app/UseCases/Campaign/CreateCampaignUseCase.js
@@ -1,29 +1,58 @@
 const { campaignStatus } = use('App/Models/Enums/Campaign');
 
+const { eventLogTypes } = use('App/Models/Enums/EventsLogs');
+
 const NotFoundException = use('App/Exceptions/NotFoundException');
 class CreateCampaignUseCase {
   // Injeta dependências no construtor
   static get inject() {
-    return ['App/Models/Campaign', 'App/Models/Institution'];
+    return ['App/Models/Campaign', 'App/Models/Institution', 'App/Models/CampaignEventsLog', 'App/Models/Shared/UnitOfWork'];
   }
 
   // Recebe dependências declaradas no inject
-  constructor(campaignModel, institutionModel) {
+  constructor(campaignModel, institutionModel, campaignEventsLogModel, uow) {
     this.campaignModel = campaignModel;
+    this.campaignEventsLogModel = campaignEventsLogModel;
     this.institutionModel = institutionModel;
+    this.uow = uow;
   }
 
   async execute(campaignData) {
-    const institution = await this.institutionModel.find(campaignData.referred_institution);
+    
+    // Verifica se a instituição onde a campanha será criada existe
+    const institution = await this.institutionModel.find(campaignData.institution_id);
     if (!institution) {
       return { success: false, data: new NotFoundException('Instituição não existente') };
     }
 
-    const campaign = await this.campaignModel.create(
-      { ...campaignData, status: campaignStatus.underReview },
-    );
+    // Inicia a transaction que gerencia a criação de campanha
+    try {
+      await this.uow.startTransaction();
+      
+      const campaign = await this.campaignModel.create(
+        {
+          ...campaignData, 
+          status: campaignStatus.underReview
+        },
+        this.uow.transaction,
+      );
 
-    return { success: true, data: campaign };
+      await this.uow.campaignEventsLogModel.create(
+        {
+          event_type: eventLogTypes.create,
+          data: campaign.toJSON(),
+          campaign_id: campaign.campaign_id,
+        },
+        this.uow.transaction,
+      );
+      await this.uow.commitTransction();
+
+      return campaign;
+    } catch (error) {
+      await this.uow.rollbackTransaction();
+      
+      throw error;
+    }
   }
 }
 
---
+++ database/migrations/1629332798887_campaign_events_log_schema.js
@@ -0,0 +1,31 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema');
+const { eventLogStatus } = use('App/Models/Enums/EventsLogs');
+
+class CampaignEventsLogSchema extends Schema {
+  up () {
+    this.create('campaign_events_logs', (table) => {
+      table.increments();
+      table.integer('event_type').notNullable();
+      table.json('data');
+      table.integer('status').notNullable().defaultTo(eventLogStatus.UnderReview);
+      table.text('curator_review');
+
+      table.integer('campaign_id')
+        .notNullable()
+        .references('id')
+        .inTable('campaigns');
+
+
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('campaign_events_logs')
+  }
+}
+
+module.exports = CampaignEventsLogSchema
