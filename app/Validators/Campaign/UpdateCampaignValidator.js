599384a2e1b62f58448ce5680bad76363a033f2c
jotasv <jsveleda@inf.ufsm.br>
2021-08-20 16:10:47 -0300

    validadores adicionados em atualiza campanha

--- app/Validators/Campaign/UpdateCampaignValidator.js
+++ app/Validators/Campaign/UpdateCampaignValidator.js
@@ -3,26 +3,46 @@ const BaseValidator = require('../BaseValidator');
 class UpdateCampaignValidator extends BaseValidator {
   get rules() {
     return {
-      title: 'string',
-      description: 'string',
-      address: 'string',
-      address_number: 'string',
-      address_complement: 'string',
-      neighborhood: 'string',
-      postal_code: 'length:8|string|onlyDigits',
-      state: 'length:2',
-      city: 'string',
-      address_latitude: 'string',
-      address_longitude: 'string',
+      title: 'required|string',
+      description: 'required|string',
+      address: 'required|string',
+      address_number: 'required|string',
+      address_complement: 'required|string',
+      neighborhood: 'required|string',
+      postal_code: 'required|length:8|string|onlyDigits',
+      state: 'required|length:2',
+      city: 'required|string',
+      address_latitude: 'required|string',
+      address_longitude: 'required|string',
     };
   }
 
   get messages() {
     return {
+      'title.required': 'Título é obrigatório',
+
+      'description.required': 'Descrição é obrigatória',
+
+      'address.required': 'Endereço é obrigatório',
+
+      'address_number.required': 'Número de endereço é obrigatório',
+
+      'address_complement.required': 'Complemento do endereço é obrigatório',
+
+      'neighborhood.required': 'Bairro é obrigatório',
+
+      'postal_code.required': 'CEP é obrigatório',
       'postal_code.length': 'CEP precisa ter {{argument.0}} dígitos',
       'postal_code.onlyDigits': 'Apenas dígitos são aceitos no CEP',
 
+      'state.required': 'Estado é obrigatório',
       'state.length': 'Estado precisa ter 2 letras',
+
+      'city.required': 'Cidade é obrigatório',
+
+      'address_latitude.required': 'Latitude do endereço é obrigatório',
+
+      'address_longitude.required': 'Longitude do endereço é obrigatório',
     };
   }
 }
