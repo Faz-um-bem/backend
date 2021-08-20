9ee54b96c3614bb220137908c1876cf8dabc3d82
jotasv <jsveleda@inf.ufsm.br>
2021-08-19 22:51:01 -0300

    inserida rota de deleção de campanha

--- routes/Campaign/index.js
+++ routes/Campaign/index.js
@@ -11,6 +11,8 @@ class CampaignRoutes extends BaseRoutes {
     router.get('/institutions/:id/campaign/:campaign_id', `${this.controllersPath}/Campaign/GetCampaignController.handle`);
     router.get('/campaigns', `${this.controllersPath}/Campaign/GetCampaignsController.handle`);
     router.get('/institutions/:id/campaigns', `${this.controllersPath}/Campaign/GetCampaignsByInstitutionController.handle`);
+
+    router.delete('/institutions/:id/campaign/:campaign_id', `${this.controllersPath}/Campaign/DeleteCampaignController.handle`);
   }
 }
 
