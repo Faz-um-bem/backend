bc25472caf6d380eb61dcfef2800efbb351c4a0a
jotasv <jsveleda@inf.ufsm.br>
2021-08-20 22:30:12 -0300

    busca por slug sem id da instituição

--- app/UseCases/Campaign/GetCampaignsBySlugUseCase.js
+++ app/UseCases/Campaign/GetCampaignsBySlugUseCase.js
@@ -10,14 +10,8 @@ class GetCampaignsBySlugUseCase {
     this.institutionModel = institutionModel;
   }
 
-  async execute(request) {
-    const institution = await this.institutionModel.find(request.id);
-
-    if (!institution) {
-      return { success: false, data: new NotFoundException('Instituição não encontrada') };
-    }
-
-    const campaign = await this.campaignModel.findBy('slug', request.slug);
+  async execute(requestData) {
+    const campaign = await this.campaignModel.findBy('slug', requestData.slug);
 
     if (!campaign) {
       return { success: false, data: new NotFoundException('Campanha não encontrada') };
