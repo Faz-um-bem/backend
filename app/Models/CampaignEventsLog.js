294c2ef06b294e68b22582e18b010454fade8b98
jotasv <jsveleda@inf.ufsm.br>
2021-09-02 12:29:03 -0300

    listagem com event log

--- app/Models/CampaignEventsLog.js
+++ app/Models/CampaignEventsLog.js
@@ -1,6 +1,9 @@
 const BaseModel = use('App/Models/BaseModel');
 
 class CampaignEventsLog extends BaseModel {
+  campaign() {
+    return this.belongsTo('App/Models/Campaign');
+  }
 }
 
 module.exports = CampaignEventsLog;
