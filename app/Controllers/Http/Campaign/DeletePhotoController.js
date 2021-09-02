4f7686f86c4594fd78dd39767b51300441660064
pedrodeandrade <pedroandrade.dev@gmail.com>
2021-09-02 04:39:40 -0300

    Impletado endpoint para exclusao de imagens de campanha

---
+++ app/Controllers/Http/Campaign/DeletePhotoController.js
@@ -0,0 +1,27 @@
+const BaseController = use('App/Controllers/Http/BaseController');
+const { handleError, ok } = use('App/Controllers/Http/HttpResponses');
+
+class DeletePhotoController extends BaseController {
+  static get inject() {
+    return ['App/UseCases/Campaign/DeletePhotoUseCase'];
+  }
+
+  constructor(useCase) {
+    super();
+
+    this.useCase = useCase;
+  }
+
+  async controllerOperation(requestData) {
+    const result = await this.useCase.execute(
+      { campaignId: requestData.id, photoId: requestData.photoId },
+    );
+
+    if (!result.success)
+      return handleError(result.data);
+
+    return ok(result.data);
+  }
+}
+
+module.exports = DeletePhotoController;
