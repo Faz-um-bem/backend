92fd3086bffd086860441950f7588b57855b39c9
jotasv <jsveleda@inf.ufsm.br>
2021-09-02 05:11:38 -0300

    pedido de atualização de campanha

---
+++ app/Controllers/RequestModels/Campaign/RequestCampaignUpdateRequestModel.js
@@ -0,0 +1,19 @@
+class RequestCampaignUpdateRequestModel {
+  constructor(requestCampaignUpdateRequestModel) {
+    this.title = requestCampaignUpdateRequestModel.title;
+    this.description = requestCampaignUpdateRequestModel.description;
+    this.address = requestCampaignUpdateRequestModel.address;
+    this.address_number = requestCampaignUpdateRequestModel.address_number;
+    this.address_complement = requestCampaignUpdateRequestModel.address_complement;
+    this.neighborhood = requestCampaignUpdateRequestModel.neighborhood;
+    this.postal_code = requestCampaignUpdateRequestModel.postal_code;
+    this.state = requestCampaignUpdateRequestModel.state;
+    this.city = requestCampaignUpdateRequestModel.city;
+    this.address_latitude = requestCampaignUpdateRequestModel.address_latitude;
+    this.address_longitude = requestCampaignUpdateRequestModel.address_longitude;
+    this.institution_id = requestCampaignUpdateRequestModel.id;
+    this.id = requestCampaignUpdateRequestModel.campaign_id;
+  }
+}
+
+module.exports = RequestCampaignUpdateRequestModel;
